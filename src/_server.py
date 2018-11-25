6c880a96eac21d8b4bbcaf4454c71767b38e71a9
zeziba <owengen@gmail.com>
2018-11-25 02:12:04 -0700

    Added to daemonizer

---
+++ src/_server.py
@@ -0,0 +1,30 @@
+import os
+import socketserver
+from http.server import SimpleHTTPRequestHandler
+
+os.chdir('/home/ubuntu/pybmp180/pyscript')
+address = ''
+port = 8000
+
+
+class CORSRequestHandler (SimpleHTTPRequestHandler):
+    def __int__(self, *args, **kwargs):
+        super().__init__(*args, directory=address, **kwargs)
+
+    def do_GET(self):
+        super(CORSRequestHandler, self).do_GET()
+
+    def do_OPTIONS(self):
+        self.send_header("Access-Control-Allow-Origin", "*")
+        self.send_header("Access-Control-Allow-Credentials", "true")
+        self.send_header("Access-Control-Allow-Methods", "GET")
+        self.send_header("Access-Control-Allow-Headers", "dataType, accept, authorization")
+
+    def end_headers(self):
+        self.do_OPTIONS()
+        super().end_headers()
+
+
+def main():
+    httpd = socketserver.TCPServer((address, port), CORSRequestHandler)
+    httpd.serve_forever()
