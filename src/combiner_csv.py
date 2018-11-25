08ec982776c167a8f924cc11971a2cd8405bb5e5
zeziba <owengen@gmail.com>
2018-11-25 02:56:52 -0700

    Combines the csv files from pervious versions into the database.

---
+++ src/combiner_csv.py
@@ -0,0 +1,22 @@
+"""
+This file is intended to add all the csv entries into the main sqlite file.
+"""
+
+import database
+import csv
+import os
+
+_path_ = '/home/ubuntu/pybmp180/pyscript/data'
+
+
+with database.DatabaseManager() as db:
+    for file in os.listdir(_path_):
+        if 'csv' in file:
+            _p = os.path.join(_path_, file)
+            print(_p)
+            with open(_p, 'rt', encoding='utf-8') as _csv:
+                dr = csv.DictReader(_csv)
+                for line in dr:
+                    db.add_data(line['Time(date)'], line[' degree'], line[' df'],
+                                line[' pascals'], line[' hectopascals'], line[' humidity'])
+            db.commit()
