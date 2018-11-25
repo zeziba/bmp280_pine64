9f093ccd2fda3a237f34ce160ca5c26de2e95393
zeziba <owengen@gmail.com>
2018-11-25 02:12:20 -0700

    Daemonizer for the simple-server

--- src/server.py
+++ src/server.py
@@ -1,29 +1,145 @@
+#!/usr/bin/env python3
+
+import atexit
 import os
-import socketserver
-from http.server import SimpleHTTPRequestHandler
+import sys
+import time
+from os.path import join
+from signal import signal, SIGTERM
 
-os.chdir('/home/ubuntu/pybmp180/pyscript')
-address = ''
-port = 8000
+sys.path.append('/home/ubuntu/pybmp180/pyscript')
 
+import _server as server
 
-class CORSRequestHandler (SimpleHTTPRequestHandler):
-    def __int__(self, *args, **kwargs):
-        super().__init__(*args, directory=address, **kwargs)
-
-    def do_GET(self):
-        super(CORSRequestHandler, self).do_GET()
-
-    def do_OPTIONS(self):
-        self.send_header("Access-Control-Allow-Origin", "*")
-        self.send_header("Access-Control-Allow-Credentials", "true")
-        self.send_header("Access-Control-Allow-Methods", "GET")
-        self.send_header("Access-Control-Allow-Headers", "dataType, accept, authorization")
-
-    def end_headers(self):
-        self.do_OPTIONS()
-        super().end_headers()
+__base__ = '/var/run'
 
 
-httpd = socketserver.TCPServer((address, port), CORSRequestHandler)
-httpd.serve_forever()
+class Daemon:
+    """
+    A generic daemon class.
+    Usage: subclass the Daemon class and override the run() method
+    """
+
+    def __init__(self, pidfile=join(__base__, 'simple-server.pid')):
+        self.pidfile = pidfile
+
+    def daemonize(self):
+        try:
+            pid = os.fork()
+            if pid > 0:
+                # exit first parent
+                sys.exit(0)
+        except OSError as e:
+            sys.stderr.write("fork #1 failed: %d (%s)\n" % (e.errno, e.strerror))
+            sys.exit(1)
+
+        # decouple from parent environment
+        os.setsid()
+        os.umask(0)
+
+        # do second fork
+        try:
+            pid = os.fork()
+            if pid > 0:
+                # exit from second parent
+                sys.exit(0)
+        except OSError as e:
+            sys.stderr.write("fork #2 failed: %d (%s)\n" % (e.errno, e.strerror))
+            sys.exit(1)
+
+        atexit.register(self.onstop)
+        signal(SIGTERM, lambda signum, stack_frame: exit())
+
+        # write pidfile
+        pid = str(os.getpid())
+        open(self.pidfile, 'w+').write("%s\n" % pid)
+
+    def onstop(self):
+        self.quit()
+        os.remove(self.pidfile)
+
+    def start(self):
+        """
+        Start the daemon
+        """
+        # Check for a pidfile to see if the daemon already runs
+        try:
+            pf = open(self.pidfile, 'r')
+            pid = int(pf.read().strip())
+            pf.close()
+        except IOError:
+            pid = None
+
+        if pid:
+            message = "pidfile %s already exist. Daemon already running?\nPID:{}\n".format(pid)
+            sys.stderr.write(message % self.pidfile)
+            sys.exit(1)
+
+        # Start the daemon
+        self.daemonize()
+        self.run()
+
+    def stop(self):
+        """
+        Stop the daemon
+        """
+        # Get the pid from the pidfile
+        try:
+            pf = open(self.pidfile, 'r')
+            pid = int(pf.read().strip())
+            pf.close()
+        except IOError:
+            pid = None
+
+        if not pid:
+            message = "pidfile %s does not exist. Daemon not running?\n"
+            sys.stderr.write(message % self.pidfile)
+            return  # not an error in a restart
+
+        # Try killing the daemon process
+        try:
+            while 1:
+                os.kill(pid, SIGTERM)
+                time.sleep(0.1)
+        except OSError as err:
+            err = str(err)
+            if err.find("No such process") > 0:
+                if os.path.exists(self.pidfile):
+                    os.remove(self.pidfile)
+            else:
+                print(str(err))
+                sys.exit(1)
+
+    def restart(self):
+        """
+        Restart the daemon
+        """
+        self.stop()
+        self.start()
+
+    def run(self):
+        """
+        Should be override, is run after the class is dameonized
+        :return:
+        """
+        pass
+
+    def quit(self):
+        """
+        Should be overridden, is used to quit the daemon
+        :return:
+        """
+        pass
+
+
+class Server(Daemon):
+    def run(self):
+        self.server = server
+        self.server.main()
+
+    def quit(self):
+        self.server.signal_watch.kill = True
+
+
+_daemon = Server()
+_daemon.start()
